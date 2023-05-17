e0f917968f17b29c7371fffe9dc743a0bc285659
Sourodeep chakraborty website <chakrabortysourodeep16@gmail.com>
2023-05-17 15:46:26 +0530

    gallary commit

--- gallery.php
+++ gallery.php
@@ -1,7 +1,144 @@
-<?php require_once("head.php");?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>KLANA POLYTECHNIC | Gallery</title>
+    <!--Style Starts-->
+    <link rel="icon" type="image/png" href="img/WBSCTE.jpeg">
+    <link href='https://fonts.googleapis.com/css?family=Oxygen:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+</head>
 
 <body id="gallery">
-<?php require_once("navbar.php");?>
+<!-- Start Your Body Code From here -->
+<div class="wrapper">
+    <!-- header starts here -->
+    <header id="header" class="">
+        <!-- header -->
+        <div class="header-wrapper">
+            <div class="header-container">
+                <div>
+                    <div class="logo-name">
+                        <div class="logo">
+                            <a href="index.html" title=""><img src="img/WBSCTE.jpeg" alt="logo"></a>
+                        </div>
+                        <div class="name">
+                            <a href="index.html" title="">
+                                <h5>KALNA POLYTECHNIC</h5>
+                                <h5 class="primary-color">West Bengal</h5>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="header-links">
+                    <a id="loc_icon" href="#" data-toggle="popover" title="Address"
+                       data-content="P.O.- Kalnadistrict - Burdwan Kalna Bardhaman West Bengal India 713409"
+                       data-placement="bottom"><i class="fa fa-map-marker fa-lg" aria-hidden="true"></i></a>
+                    <a id="phone_icon" href="#" data-toggle="popover" title="Phone No." data-content="+91-77-97286993"
+                       data-placement="bottom"><i class="fa fa-phone fa-lg" aria-hidden="true"></i></a>
+                    <a id="email_icon" href="#" data-toggle="popover" title="Email"
+                       data-content="chakrabortysourodeep16@gmail.com" data-placement="bottom"><i
+                            class="fa fa-envelope fa-lg" aria-hidden="true"></i></a>
+                    <a id="email_icon" href="#" data-toggle="popover" title="Website"
+                       data-content="www.kalnapolytechnic.org" data-placement="bottom"><i class="fa fa-globe fa-lg" aria-hidden="true"></i></a>
+
+                </div>
+            </div>
+        </div>
+        <nav>
+            <div class="navbar navbar-inverse navbar-custom" role="navigation">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse"
+                                data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    <div class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav">
+                            <li>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Institute <b
+                                        class="caret"></b></a>
+                                <ul class="dropdown-menu multi-level">
+                                    <li><a href="about.html">About</a></li>
+                                    <li>
+                                        <a href="society_members.html">Society Members</a>
+                                    </li>
+                                    <li>
+                                        <a href="academic_council.html">Academic Council</a>
+                                    </li>
+                                    <li>
+                                        <a href="finance_committee.html">Finance Committee</a>
+                                    </li>
+                                    <li><a href="rti.html">RTI</a></li>
+                                    <li class="dropdown-submenu">
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Linkages</a>
+                                        <ul class="dropdown-menu"></ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Academics <b
+                                        class="caret"></b></a>
+                                <ul class="dropdown-menu multi-level">
+                                    <li class="dropdown-submenu">
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Diploma</a>
+                                        <ul class="dropdown-menu">
+                                            <li><a href="#!">CST</a></li>
+                                            <li><a href="#!">ETC</a></li>
+                                            <li><a href="#!">CE</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#!">Academic Calender</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Admission <b
+                                        class="caret"></b></a>
+                                <ul class="dropdown-menu multi-level">
+                                    <li><a href="https://webscte.co.in/">Click Hare To Go WBSCTE official Website</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">People <b
+                                        class="caret"></b></a>
+                                <ul class="dropdown-menu multi-level">
+                                    <li><a href="faculty.html">Faculty</a></li>
+                                    <li><a href="staff.html">Staff</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">More <b
+                                        class="caret"></b></a>
+                                <ul class="dropdown-menu multi-level">
+                                    <li>
+                                        <a href="student_corner.html">Student's Corner</a>
+                                    </li>
+                                    <li><a href="library.html">Library</a></li>
+                                    <li><a href="file_download.html">Download Diploma 2nd sem CST marksheet</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="gallery.html">Photo Gallery</a></li>
+                            <li>  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Login <b
+                                class="caret"></b></a>
+                        <ul class="dropdown-menu multi-level">
+                            <li><a href="#!">Student Login</a></li>
+                            <li><a href="#!">Admin Login</a></li>
+                        </ul>
+                        </li>
+                        </ul>
+                    </div>
+                    <!--/.nav-collapse -->
+                </div>
+            </div>
+        </nav>
+    </header>
     <!-- /header -->
     <!-- Content wrapper -->
     <div class="contents-wrapper">
@@ -57,6 +194,115 @@
         </div>
     </div>
     <!-- Footer -->
-<?php require_once("footer.php");?>
+    <footer>
+        <div class="footer-wrapper">
+            <span class="logo"><img src="img/WBSCTE.jpeg" style="border-radius: 20px;" alt=""></span>
+            <div class="row">
+                <div class="col-sm-4 address">
+                    <strong>College name:</strong> Kalna Polytechnic College <br>
+                    <strong>Address : </strong> P.O.- Kalnadistrict - Burdwan Kalna Bardhaman West Bengal India 713409
+                </div>
+                <div class="col-sm-4 links">
+                    <div>
+                        <a href="index.html#news">News</a>
+                        <i class="seperator"></i>
+                        <a href="index.html#announcements">Announcement</a>
+                        <i class="seperator"></i>
+                        <a href="index.html#events">Events</a>
+                    </div>
+                    <div>
+                        <a href="index.html">Home</a>
+                        <i class="seperator"></i>
+                        <a href="rti.html">RTI</a>
+                        <i class="seperator"></i>
+                    </div>
+                    <div>
+                        <a href="admissions.html">Admission</a>
+                        <i class="seperator"></i>
+                        <a href="student_corner.html">Student</a>
+                        <i class="seperator"></i>
+                        <a href="faculty.html">Faculty</a>
+                    </div>
+                </div>
+                <div class="col-sm-4 footer-social">
+                    <a href="https://www.facebook.com/groups/kplovesu"><i class="fa fa-facebook" aria-hidden="true"
+                                    title="Official Facebook Page"></i></a>
+                    <a href="https://github.com/Sourodeep01001"><i class="fa fa-github" aria-hidden="true"
+                                                          title="Github Group"></i></a>
+                    <a href="#!"><i class="fa fa-linkedin" aria-hidden="true" title="LinkedIn Profile"></i></a>
+                    <a href="#!" title=""><i class="fa fa-twitter" aria-hidden="true"
+                                             title="Official Twitter Account"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+<!--Scripts Starts-->
+<script   src="https://code.jquery.com/jquery-2.2.4.min.js"   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="   crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<script src="https://use.fontawesome.com/434d359724.js"></script>
+<script src="js/imagesloaded.pkgd.js"></script>
+<script src="https://npmcdn.com/masonry-layout@4.0/dist/masonry.pkgd.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script>
+    $(document).ready(function () {
+        var $grid = $('.grid').imagesLoaded(function () {
+            $grid.masonry({
+                itemSelector: '.grid-item',
+                percentPosition: true,
+                columnWidth: '.grid-sizer'
+            });
+        });
+
+        $('.grid').magnificPopup({
+            delegate: 'a', // child items selector, by clicking on it popup will open
+            type: 'image',
+            tLoading: 'Loading image #%curr%...',
+            mainClass: 'mfp-img-mobile',
+            zoom: {
+                enabled: true,
+                duration: 300,
+                easing: 'ease-in-out'
+            },
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+            },
+            image: {
+                verticalFit: true
+            }
+            // other options
+        });
+    })
+</script>
+<script>
+    $(document).on('click', function (e) {
+        var
+                $popover,
+                $target = $(e.target);
+
+        //do nothing if there was a click on popover content
+        if ($target.hasClass('popover') || $target.closest('.popover').length) {
+            return;
+        }
+
+        $('[data-toggle="popover"]').each(function () {
+            $popover = $(this);
+
+            if (!$popover.is(e.target) &&
+                    $popover.has(e.target).length === 0 &&
+                    $('.popover').has(e.target).length === 0) {
+                $popover.popover('hide');
+            } else {
+                //fixes issue described above
+                $popover.popover('toggle');
+            }
+        });
+    })
+
+
+</script>
+<script src="//code.tidio.co/2bdobyictggzuaq4y4gjo4mk7gbtvhqb.js" async></script>
 </body>
 </html>
